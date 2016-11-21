7c76ae24b68a5fa6f20f945bfc377fbc41566b05
Michael Demesy <michaeldemesy@gmail.com>
2016-11-21 22:01:57 +0100

    Add files via upload

---
+++ src/Models/ModelDe.java
@@ -0,0 +1,6 @@
+import java.util.concurrent.ThreadLocalRandom;
+public class ModelDe {
+	private static String deEvent[] = {"Action 1", "Action 2", "Action 3","Action 4","Action 5", "Action 6"};
+	public static int lanceDe() {	return ThreadLocalRandom.current().nextInt(1, 6 + 1);	}
+	public static String getAction() {	return deEvent[lanceDe()-1];	}
+}
