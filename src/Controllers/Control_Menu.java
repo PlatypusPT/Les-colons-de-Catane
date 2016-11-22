fc7a72f42457a63b34b69704f6ce8c50c6031f28
Yoann HAFFNER <yoyofrom68@gmail.com>
2016-11-22 08:40:14 +0100

    Add files via upload

--- src/Controllers/Control_Menu.java
+++ src/Controllers/Control_Menu.java
@@ -3,10 +3,15 @@ package Controllers;
 import Models.Model;
 import Views.Menu_View;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 /**
  * Created by yhaffner on 21/11/16.
  */
-public class Control_Menu {
+public class Control_Menu implements MouseListener {
     private Menu_View view;
     private Model model;
 
@@ -14,5 +19,34 @@ public class Control_Menu {
         this.model = model;
         this.view = view;
         view.setController(this);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if(view.startButton.equals(e.getSource()) || view.optionButton.equals(e.getSource()))
+            ((JPanel)e.getSource()).setBackground(new Color(70,10,10));
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        if(view.startButton.equals(e.getSource()) || view.optionButton.equals(e.getSource()))
+            ((JPanel)e.getSource()).setBackground(new Color(124,28,37));
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        if(view.startButton.equals(e.getSource()) || view.optionButton.equals(e.getSource()))
+            ((JPanel)e.getSource()).setBackground(new Color(148,34,44));
+
     }
 }
