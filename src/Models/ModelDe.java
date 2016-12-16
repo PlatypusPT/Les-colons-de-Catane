f042008afabb5f0ddda7d5af34a345b02eb9ab75
Alessio90 <alex.chevroton@outlook.fr>
2016-12-16 09:39:04 +0100

    Cette fois, c'est la bonne !!

--- src/Models/ModelDe.java
+++ src/Models/ModelDe.java
@@ -6,9 +6,16 @@ public class ModelDe {
 
 	private static String deEvent[] = {"Action 1", "Action 2", "Action 3","Action 4","Action 5", "Action 6"};
 
+	/*
+	Lancé du dé de production
+	 */
 	public static int lancerDeProduction() {
 	    return ThreadLocalRandom.current().nextInt(1, 6 + 1);
 	}
+
+	/*
+	Lancé du dé événement
+	 */
 	public static String lancerDeEvenement() {
 	    return deEvent[ThreadLocalRandom.current().nextInt(1, 6 + 1)];
 	}
