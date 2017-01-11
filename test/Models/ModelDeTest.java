cd1a1a3e8036f3d1c72d7f6111f3557f48b3411d
Alessio90 <alex.chevroton@outlook.fr>
2017-01-11 15:20:10 +0100

    Mes premiers tests, d'autres sont à venir très bientôt !! :)

--- test/Models/ModelDeTest.java
+++ test/Models/ModelDeTest.java
@@ -1,16 +1,16 @@
 package Models;
 import org.junit.*;
 
-
 /**
  * Created by mdemesy on 22/11/16.
  */
 public class ModelDeTest {
+
     @Test
     public void testDeComprisEntreUnEtSix() {
         for (int i=0;i<100;i++) {
             int de = ModelDe.lancerDeProduction();
-            Assert.assertTrue(de <= 6 && de >= 1);
+            Assert.assertTrue(de <= 5 && de >= 0);
         }
     }
 
--- test/Models/TerrainUnitTest.java
+++ test/Models/TerrainUnitTest.java
@@ -1,6 +1,7 @@
 package Models;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -8,5 +9,46 @@ import org.junit.Test;
  */
 public class TerrainUnitTest {
 
+    Terrain terrain = new Terrain("Blé", 1, 1, null);
 
+    @Test
+    public void construireUnTerraintUnitTest(){
+        Assert.assertEquals(terrain.getRessource(),"Blé");
+        Assert.assertEquals(terrain.getNbRessource(), 1);
+        Assert.assertEquals(terrain.getFaceDé(), 1);
+        Assert.assertEquals(terrain.batimentProdOuPas(), false);
+        Assert.assertEquals(terrain.yaUneFlotteOuPas(), false);
+        Assert.assertEquals(terrain.estIlProtegeOuPas(), false);
+        Assert.assertEquals(terrain.yaUnBainOuPas(), false);
+    }
+
+    @Test
+    public void ajouterUneRessourceUnitTest(){
+        terrain.setNbRessource(2);
+        Assert.assertEquals(terrain.getNbRessource(), 2);
+    }
+
+    @Test
+    public void construireUnBatimentQuiDoubleLaProductionUnitTest(){
+        terrain.doublerProduction();
+        Assert.assertEquals(terrain.batimentProdOuPas(), true);
+    }
+
+    @Test
+    public void mettreUneFlotteDeCommerceUnitTest(){
+        terrain.mettreUneFlotteCommerce();
+        Assert.assertEquals(terrain.yaUneFlotteOuPas(), true);
+    }
+
+    @Test
+    public void construireUnFortUnitTest(){
+        terrain.proteger();
+        Assert.assertEquals(terrain.estIlProtegeOuPas(), true);
+    }
+
+    @Test
+    public void construireUnBainUnitTest(){
+        terrain.ajouterBain();
+        Assert.assertEquals(terrain.yaUnBainOuPas(), true);
+    }
 }
